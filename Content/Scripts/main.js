9f95e6266b73f7428fea0819ac125e817b5a98d2
Trent Gutting <tgutting@247sports.com>
2014-09-17 14:14:10 -0500

    Started a player object.

--- Content/Scripts/main.js
+++ Content/Scripts/main.js
@@ -7,5 +7,32 @@ $(document).ready(function () {
 });
 (function(){
 //Get your angular on down here
-var game = angular.module('CompositeClicker', []);
+var app = angular.module('compositeClicker', []);
+
+
+app.controller('GameController', function()
+{	
+	//Keep the variables encapsulated like this
+	var _game = {displayTitle: true};	
+	this.showTitle = function()
+	{
+		return _game.displayTitle;
+	};
+	this.startGame = function()
+	{	
+		_game.displayTitle ^= true;
+	};
+});
+
+app.controller('PlayerController', function()
+{
+	var _player = {
+		Name: 'Trent',
+		HeightFeet: 6,
+		HeightInches: 6,
+		Weight: 290
+	};
+	this.player = _player;
+});
+
 })();
